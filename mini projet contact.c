3b53b991907e1488dad4644ae4616330e82cc048
AMINE ELHAILAA <ael46041@gmail.com>
2025-09-28 22:54:32 +0100

    finir la premiere version du projet de contact
    first version

--- mini projet contact.c
+++ mini projet contact.c
@@ -6,33 +6,45 @@ struct contact {
     char phone[99];
     char mail[99];
 };
-
-// int search(char keyword[],char list[]){
-//     if(strstr(search[],list[])==true){
-//         return 1;
-//     }
-//     else {
-//         return 0;
-//     }
-
-// }
+struct contact person[100];
 
 void scan(char string[99]) { //instead of scanf
     scanf(" %[^\n]",string);
 }
 
-void show(struct contact person[], int i) { //affichage
+int user_input() {  //function for any typo from user in integer inputs
+    char newline_check;
+    int choix;
+    while (1) {
+        if (scanf("%d%c", &choix,&newline_check)==2 && newline_check =='\n') { //scanf the integer and the first character
+            return choix;
+        }
+        else {
+            while (getchar()!='\n') {
+
+            }
+            return -1;
+        }
+    }
+}
+
+void fgets_char(char string[],int size) {
+    fgets(string,size,stdin);
+    string[strlen(string)-1]='\0';
+}
+
+void show( int i) { //affichage
     // printf("-------------------index-%03d------------------\n", i);
     // printf("le nom=======> %s\n", person[i].nom);
     // printf("le numero====> %s\n", person[i].phone);
     // printf("l'email======> %s\n", person[i].mail);
-    printf("---------------------------------------------------------------------------------------\n");
-    printf("index | %-20s | %-20s  | %-30s |\n","Nom","Numero","Email");
-    printf(" %03d  | %-20s | 0%-20s | %-30s |\n",i,person[i].nom,person[i].phone,person[i].mail);
+    // printf("---------------------------------------------------------------------------------\n");
+    printf("| %-20s | %-20s  | %-30s \n","Nom","Numero","Email");
+    printf("| %-20s | 0%-20s | %-30s \n",person[i].nom,person[i].phone,person[i].mail);
 }
 
 
-int find(struct contact person[], char keyword[], int y) { //search function
+int find(char keyword[],int y) { //search function
     if (strstr(person[y].nom, keyword) !=NULL ) {
         return y;
         }
@@ -43,9 +55,8 @@ int find(struct contact person[], char keyword[], int y) { //search function
 
 
 int main() {
-    struct contact person[100];
-    int a;
-    int nbr_contact = -1;
+
+    int choix , nbr_contact= -1;
     do {
         printf("\n\n\n1. Ajouter un Contact\n");
         printf("2. Modifier un Contact\n");
@@ -54,12 +65,15 @@ int main() {
         printf("5. Rechercher un Contact\n");
         printf("6. exit\n");
         printf("====> ");
-        if (scanf(" %d", &a)!=1) {
-            scanf("%*s");
-            a=7; // 7 just to send it to default ...
-        }
+        choix = user_input();
+        // if (scanf("%d%c", &choix,&newline)==2 && newline =='\n') { //scanf the integer and the first character
+        // }
+        // else {
+        //     scanf(" %*[^\n]");
+        //     choix=7;
+        // }
 
-        switch (a) {
+        switch (choix) {
             case 1: //ad
             {
                 nbr_contact++;
@@ -73,7 +87,7 @@ int main() {
                 // scanf(" %[^\n]", person[nbr_contact].mail);
                 scan(person[nbr_contact].mail);
                 printf("==========contact succesfully added================\n");
-                show(person, nbr_contact);
+                show(nbr_contact);
 
                 break;
             } //done
@@ -82,32 +96,39 @@ int main() {
                 char temp_nom[99];
                 int index_store[99];
                 int index=-1,bool=0;
-                printf("entrez le contact que vous voulez medifier: ");
+                printf("entrez le contact que vous voulez supprimer: ");
                 scan(temp_nom);
 
                 for (int i=0; i<= nbr_contact; i++) {
-                    if (find(person, temp_nom,i) > -1) {
-                        show(person, find(person, temp_nom,i)); //showing contact with this term search
+                    if (find(temp_nom,i) > -1) {
+
                         index++;
                         index_store[index]=i;   //storing the indexs of contact with that term search in a new array
                         bool=1;
+                        printf("Contact:%d\n",index);
+                        show(find(temp_nom,i)); //showing contact with this term search
                     }
                 }
-                if (bool==0) printf("contact introuvable");
-                printf("choisir l'index du contact que vous voulez medifier ");
-                int input;
-                scanf("%d",&input);
-                if (input<=index) {
+                if (bool==0) {
+                    printf("contact introuvable");
+                    break;//si tas pas trouve le contact quitte  la choix.
+                }
+                printf("choisir l'index du contact que vous voulez supprimer==> ");
+                int input=user_input();
+                // scanf("%d",&input);
+
+                int temp=index_store[input]; //i got the the index where we will start shifting
+                if (input >= 0 && input<=index) {
                     nbr_contact--;
-                    for (int i=input;i<= nbr_contact;i++) {
+                    for (int i=temp;i<= nbr_contact;i++) {
                         person[i]=person[i+1];
 
                     }
+                    printf("le contact a ete supprimer! \n");
                 }
                 else {
                     printf("le choix entrer est invalid!\n");
                 }
-
                 break;
 
 
@@ -115,14 +136,56 @@ int main() {
 
             case 2: //edit
             {
-                
+                char temp_nom[3][99];
+                int index_store[99];
+                int index=-1,bool=0;
+                printf("entrez le contact que vous voulez modifier : ");
+                scan(temp_nom[0]);
+
+                for (int i=0; i<= nbr_contact; i++) {
+                    if (find(temp_nom,i) > -1) {
+
+                        index++;
+                        index_store[index]=i;   //storing the indexs of contact with that term search in a new array
+                        bool=1;
+                        printf("Contact:%d\n",index);
+                        show(find(temp_nom,i)); //showing contact with this term search
+                    }
+                }
+                if (bool==0) {
+                    printf("contact introuvable");
+                    break;//si tas pas trouve le contact quitte  la choix.
+                }
+                printf("choisir l'index du contact que vous voulez modifier==> ");
+                int input=user_input();
+                // scanf("%d",&input);
+
+                int temp=index_store[input]; //i got the the index where we will start shifting
+                if (input >= 0 && input<=index) {
+                    printf("entrez le nouveau nom ( click entrez si vous navez rien a changer ) ===>");
+                    fgets_char(temp_nom[0],99);
+                    printf("entrez le nouveau numero ( click entrez si vous navez rien a changer ) ===>");
+                    fgets_char(temp_nom[1],99);
+                    printf("entrez le nouveau email ( click entrez si vous navez rien a changer ) ===>");
+                    fgets_char(temp_nom[2],99);
+                    if (temp_nom[0][0]!= '\n') strcpy(person[temp].nom,temp_nom[0]);
+                    if (temp_nom[1][0]!= '\n') strcpy(person[temp].phone,temp_nom[1]);
+                    if (temp_nom[2][0]!= '\n') strcpy(person[temp].mail,temp_nom[2]);
+                    printf("le contact a ete modifier! \n");
+                    show(temp);
+                }
+                else {
+                    printf("le choix entrer est invalid!\n");
+                }
+                break;
             }
 
             case 4: //show
             {
                 int bool=0;
                 for (int i = 0; i <= nbr_contact; i++) {
-                    show(person, i);
+                    printf("---------------------------------------------------------------------------------\n");
+                    show(i);
                     bool=1;
                 }
                 if (bool==0) printf("ajouter un contact pour le trouver ici! \n");
@@ -136,24 +199,23 @@ int main() {
                 printf("Enterez le nom ===> ");
                 scan(temp_nom);
                 for (int i=0; i<= nbr_contact; i++) {
-                    if (find(person, temp_nom,i) > -1) {
-                        show(person, find(person, temp_nom,i));
+                    if (find(temp_nom,i) > -1) {
+                        show(find(temp_nom,i));
                         bool=1;
                     }
                 }
                 if (bool==0) printf("contact introuvable");
                 break;
-            } //done
-
-            case 6: //quit
-            {
-                break;
             }
-
             default: {
-                printf("vous devez choisir un choix !\n");
-
+                if (choix==6) {
+                    printf("Au revoir");
+                }
+                else {
+                    printf("vous devez choisir un choix !\n");
+                    break;
+                }
             }
         }
-    } while (a != 0);
+    } while (choix != 6);
 }
